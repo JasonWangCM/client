17a16bfebe69024c2f75e4ba61f2d995cbef3b0a
JasonWangCM <ywang249@gmail.com>
2023-04-26 10:08:56 -0700

    modified:   src/App.js 	new file:   src/SortableItem.js 	new file:   yarn.lock

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import { DndContext, closestCenter } from "@dnd-kit/core";
+import { arrayMove, SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
+import { useState } from "react";
+import { SortableItem } from "./SortableItem";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [languages, setLanguages] = useState(["JavaScript", "Python", "TypeScript"]);
+    const show = () => {
+        console.log("The current state is: ", languages);
+    };
+    return (
+        <DndContext collistionDetection={closestCenter} onDragEnd={handleDragEnd}>
+            {/*  */}
+            <Container className="p-3" style={{ width: "50%" }} align="center">
+                <h3>The best programming languages!</h3>
+                <button onClick={show}> show current</button>
+                <SortableContext items={languages} strategy={verticalListSortingStrategy}>
+                    {/* we need components that use the useSortable hook */}
+                    {languages.map((language) => (
+                        <SortableItem key={language} id={language} />
+                    ))}
+                </SortableContext>
+            </Container>
+        </DndContext>
+    );
+    function handleDragEnd(event) {
+        console.log("Drag end called");
+        const { active, over } = event;
+        console.log("ACTIVE: " + active.id);
+        console.log("OVER: " + over.id);
+        if (active.id !== over.id) {
+            setLanguages((items) => {
+                const activeIndex = items.indexOf(active.id);
+                const overIndex = items.indexOf(over.id);
+                // console.log(arrayMove(items, activeIndex, overIndex));
+                return arrayMove(items, activeIndex, overIndex);
+                // items:[1,2,3] => [2,3,1] ===> index:0 -> index:2
+                // [1,2,3]  oldIndex: 0  newIndex: 2 -> [2,3,1] (params for arrayMove)
+            });
+        }
+    }
 }
 
 export default App;
