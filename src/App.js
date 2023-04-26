4d9ad4783626e3a075429888cddf897fff2c8c46
JasonWangCM <ywang249@gmail.com>
2023-04-26 10:54:21 -0700

    modified:   src/App.js 	modified:   src/SortableItem.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
@@ -14,12 +14,16 @@ function App() {
     };
     return (
         <DndContext collistionDetection={closestCenter} onDragEnd={handleDragEnd}>
-            {/*  */}
+            {/* wrap in DndContext allow use dnd-kitr funcs */}
+            {/*  onDragEnd => event: finish drag and drop */}
             <Container className="p-3" style={{ width: "50%" }} align="center">
                 <h3>The best programming languages!</h3>
                 <button onClick={show}> show current</button>
                 <SortableContext items={languages} strategy={verticalListSortingStrategy}>
-                    {/* we need components that use the useSortable hook */}
+                    {/* we need components that use the useSortable hook,which allows to be interacted with by this sortableContext; drag and drop within this context*/}
+                    {/* SortableContext allows to use SortableItem; */}
+                    {/* items: requires the items to you want to show;*/}
+                    {/*strategy: how to sort the data;*/}
                     {languages.map((language) => (
                         <SortableItem key={language} id={language} />
                     ))}
@@ -28,15 +32,19 @@ function App() {
         </DndContext>
     );
     function handleDragEnd(event) {
-        console.log("Drag end called");
+        // console.log("Drag end called");
         const { active, over } = event;
-        console.log("ACTIVE: " + active.id);
-        console.log("OVER: " + over.id);
+        // todo :: active: item being draged; over item being passed
+        // console.log("ACTIVE: " + active.id);
+        // console.log("OVER: " + over.id);
+        // todo :: update the state
         if (active.id !== over.id) {
+            // check over happened
             setLanguages((items) => {
                 const activeIndex = items.indexOf(active.id);
                 const overIndex = items.indexOf(over.id);
                 // console.log(arrayMove(items, activeIndex, overIndex));
+                //!! arrayMove (3 params: array, oldIndex, newIndex)
                 return arrayMove(items, activeIndex, overIndex);
                 // items:[1,2,3] => [2,3,1] ===> index:0 -> index:2
                 // [1,2,3]  oldIndex: 0  newIndex: 2 -> [2,3,1] (params for arrayMove)
--- src/SortableItem.js
+++ src/SortableItem.js
@@ -1,11 +1,16 @@
 import { useSortable } from "@dnd-kit/sortable";
-import { CSS } from "@dnd-kit/utilities";
+import { CSS } from "@dnd-kit/utilities"; // allow create some transform
 import Card from "react-bootstrap/Card";
 
 export function SortableItem(props) {
     // props.id
     // JavaScript
 
+    // properties from useSortable
+    // attributes:
+    // listeners: onDrag, onDrop
+    // setNodeRef: tell sortable exactly which node to attach to
+    // transform, transition: help us to create picking up a card and move around the card
     const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: props.id });
 
     const style = {
@@ -13,6 +18,11 @@ export function SortableItem(props) {
         transition,
     };
     return (
+        // ref: define the the target of dragable as this <div>
+        // style: will affect how things render during the drag and drop
+        // anything within attributes object will become the property of this div
+        // anything within listeners object will become the property of this div
+        //
         <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
             <Card body className="m-3">
                 {props.id}
